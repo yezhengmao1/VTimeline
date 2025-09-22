75cf69f2b802e444d54197c8af82a55d3d1977b8
zmye <yezhengmaolove@gmail.com>
2025-09-22 11:38:49 +0800

    utils query script

--- query.py
+++ query.py
@@ -5,12 +5,8 @@ import gzip
 import json
 import os
 from dataclasses import dataclass
-from datetime import datetime
 from pathlib import Path
 from typing import Any, Dict, List, Tuple, Optional
-
-import duckdb
-import pandas
 
 
 def parse_range_list(value_list):
@@ -69,25 +65,14 @@ class TraceEvent:
 
 
 parser = argparse.ArgumentParser(
-    description="Convert the log files to duckdb for analyze"
+    description="Convert the log files to chrome trace json"
 )
 
 parser.add_argument(
     "--logdir",
     help="Input log directory",
     type=str,
-    default=None,
-)
-parser.add_argument(
-    "--db",
-    help="Output duckdb file",
-    type=str,
     required=True,
-)
-parser.add_argument(
-    "--query",
-    help="Query the duckdb file",
-    action="store_true",
 )
 parser.add_argument(
     "--rank",
@@ -168,89 +153,6 @@ def get_rank_file_from_dir(logdir: Path) -> Dict[str, Dict[str, List[str]]]:
     return result
 
 
-def create_duckdb_table():
-    if os.path.exists(f"{args.db}.duckdb"):
-        raise FileExistsError(f"{args.db}.duckdb already exists")
-
-    conn = duckdb.connect(f"{args.db}.duckdb")
-    conn.execute("""
-    CREATE TABLE tracepoint (
-        timestamp BIGINT,
-        hostname TEXT,
-        rank INTEGER,
-        stream INTEGER,
-        op_type VARCHAR,
-        op_name VARCHAR,
-        op_phase VARCHAR,
-    )""")
-    conn.execute("CREATE INDEX idx_timestamp ON tracepoint (timestamp)")
-    conn.execute("CREATE INDEX idx_rank ON tracepoint (rank)")
-    conn.execute("CREATE INDEX idx_stream ON tracepoint (stream)")
-    conn.execute("CREATE INDEX idx_op_type ON tracepoint (op_type)")
-
-    return conn
-
-
-def read_tracepoint_from_file(hostname: str, log_file: str, tsunit: str):
-    print(f"Writing {log_file} ...")
-    with open(log_file, "r") as f:
-        raw_data = pandas.read_csv(
-            f,
-            header=None,
-            names=["timestamp", "rank", "stream", "op_type", "op_name", "op_phase"],
-        )
-        raw_data.insert(1, "hostname", hostname)
-        if tsunit == "ns":
-            # convert to us
-            raw_data["timestamp"] = raw_data["timestamp"] / 1000
-    return raw_data
-
-
-def convert_tracepoint_to_duckdb():
-    log_files = get_rank_file_from_dir(args.logdir)
-    conn = create_duckdb_table()
-
-    raw_datas = []
-    for hostname, rank_files in log_files["tracepoint"].items():
-        for rank_file in rank_files:
-            raw_datas.append(read_tracepoint_from_file(hostname, rank_file, "us"))
-
-    for hostname, rank_files in log_files["cupti"].items():
-        for rank_file in rank_files:
-            raw_datas.append(read_tracepoint_from_file(hostname, rank_file, "ns"))
-
-    raw_data = pandas.concat(raw_datas)
-    conn.register("raw_data", raw_data)
-    conn.execute("INSERT INTO tracepoint SELECT * FROM raw_data")
-    conn.unregister("raw_data")
-
-    conn.close()
-
-
-def query_cupti_from_duckdb(conn: duckdb.DuckDBPyConnection):
-    conn.execute(
-        "SELECT * FROM tracepoint WHERE rank=0 and op_type='CUPTI' and (op_name='cupti-enable' or op_name='cupti-disable')"
-    )
-    for record in conn.fetchall():
-        print(
-            f"{record[5]}-{record[6]} {record[0]} <> {datetime.fromtimestamp(record[0] / 1000000).strftime('%Y-%m-%d %H:%M:%S')}"
-        )
-
-
-def query_step_from_duckdb(conn: duckdb.DuckDBPyConnection):
-    conn.execute(
-        "SELECT * FROM tracepoint WHERE rank=0 and timestamp >= {} and timestamp <= {} and op_type='Train'".format(
-            args.begin, args.end
-        )
-    )
-    for record in conn.fetchall():
-        if "train-step-it" not in record[5]:
-            continue
-        print(
-            f"{record[5]}-{record[6]} {record[0]} <> {datetime.fromtimestamp(record[0] / 1000000).strftime('%Y-%m-%d %H:%M:%S')}"
-        )
-
-
 def generate_chrome_trace_json(events: List[Dict[str, Any]]) -> Dict[str, Any]:
     """Generate Chrome Trace JSON structure."""
     return {
@@ -262,19 +164,12 @@ def generate_chrome_trace_json(events: List[Dict[str, Any]]) -> Dict[str, Any]:
     }
 
 
-def generate_chrome_trace_from_duckdb(conn: duckdb.DuckDBPyConnection):
-    rank_filter_str = ",".join(map(str, args.rank))
-    conn.execute(
-        "SELECT * FROM tracepoint WHERE rank in ({}) and timestamp >= {} and timestamp <= {}".format(
-            rank_filter_str,
-            args.begin,
-            args.end,
-        )
-    )
+def generate_chrome_trace_from_logdir():
+    all_records = []
+    # TODO: get all records from logdir
 
     all_events = []
-
-    for record in conn.fetchall():
+    for record in all_records:
         event_name = record[5]
         if args.id_map and "vtimeline_marker" in record[5]:
             if record[5].split("_")[-1] in args.id_map:
@@ -311,15 +206,9 @@ def generate_chrome_trace_from_duckdb(conn: duckdb.DuckDBPyConnection):
     return generate_chrome_trace_json(all_events)
 
 
-def query_duckdb():
-    conn = duckdb.connect(f"{args.db}.duckdb")
-
-    if args.cupti:
-        query_cupti_from_duckdb(conn)
-    elif args.query_step:
-        query_step_from_duckdb(conn)
-    elif args.output:
-        trace_json = generate_chrome_trace_from_duckdb(conn)
+if __name__ == "__main__":
+    if args.logdir:
+        trace_json = generate_chrome_trace_from_logdir()
         output_file = args.output
         if not output_file.endswith(".gz"):
             output_file += ".gz"
@@ -329,19 +218,6 @@ def query_duckdb():
             json.dump(trace_json, f, separators=(",", ":"))
 
         print(f"Open {output_file} in Chrome at https://ui.perfetto.dev/ to visualize.")
-    else:
-        print("No query specified")
-        parser.print_help()
-        parser.print_usage()
-
-    conn.close()
-
-
-if __name__ == "__main__":
-    if args.logdir:
-        convert_tracepoint_to_duckdb()
-    elif args.query:
-        query_duckdb()
     else:
         parser.print_help()
         parser.print_usage()
